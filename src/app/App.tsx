1dab0578ceb1df7fb908ccc044a0ee637be36530
oleg <oleg.hasanov@ffin.kz>
2025-10-18 14:57:44 +0300

    init commit

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,7 @@
-import { AppRouter } from "./routers"
+import { AppRouter } from './routers';
 
 function App() {
-    return (
-            <AppRouter />
-    )
+  return <AppRouter />;
 }
 
-export default App
+export default App;
