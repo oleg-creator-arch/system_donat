3d4179911397d076da4404dbaa7d59fb8fb375af
oleg <oleg.hasanov@ffin.kz>
2025-10-31 16:00:49 +0300

    Add base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/system_donat/',
   plugins: [
     react({
       babel: {
@@ -10,4 +11,4 @@ export default defineConfig({
       },
     }),
   ],
-})
+});
